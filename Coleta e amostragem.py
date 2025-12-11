81fdc88b2ec8cb43ee7ee771616ea2b0e294c098
Gabriel V Silva <47651757+GabrielVSilva25@users.noreply.github.com>
2025-12-10 23:41:35 -0300

    Add files via upload

---
+++ Coleta e amostragem.py
@@ -0,0 +1,22 @@
+# Crie um programa que solicite à pessoa usuária digitar seu nome, e imprima “Olá, [nome]!”.
+
+nome = input('Digite o seu nome: ')
+
+print(f'Olá, {nome}')
+
+# Crie um programa que solicite à pessoa usuária digitar seu nome e idade, e imprima “Olá, [nome], você tem [idade] anos.”.
+
+nome = input('Digite o seu nome: ')
+idade = int(input('Digite a sua idade: '))
+
+
+print(f'Olá, {nome}, você tem {idade} anos')
+
+# Crie um programa que solicite à pessoa usuária digitar seu nome, idade e altura em metros, e imprima “Olá, [nome], você tem [idade] anos e mede [altura] metros!”.
+
+
+nome = input('Digite o seu nome: ')
+idade = int(input('Digite a sua idade: '))
+altura = float(input('Informe a sua altura: '))
+
+print(f'Olá, {nome}, você tem {idade} anos e mede {altura}')
