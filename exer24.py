d5b97eb2af6de7b764c076e8902ad41d99e90db0
Gabriel V Silva <47651757+GabrielVSilva25@users.noreply.github.com>
2025-12-11 23:07:56 -0300

    Add files via upload

---
+++ exer24.py
@@ -0,0 +1,12 @@
+# Escreva um programa que peça à pessoa usuária para fornecer dois números e exibir o número maior.
+
+num1 = int(input('Digite um número: '))
+num2 = int(input('Digite outro número: '))
+
+if num1 > num2:
+    print(f'O maior valor informado é {num1}')
+
+else:
+    print(f'O maior valor informado é {num2}')
+
+    
